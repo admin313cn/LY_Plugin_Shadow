237e760b74bf8358be8d1c87f6f0f8f538fb0d44
admin313cn <admin@313u.cn>
2022-03-11 14:56:21 +0800

    第一次测试

---
+++ base/src/main/java/cn/u313/plugin/base/HostEnterCallback.java
@@ -0,0 +1,18 @@
+package cn.u313.plugin.base;
+
+import android.view.View;
+
+public abstract class HostEnterCallback  {
+
+        public void onShowLoadingView(View view) {
+
+        }
+
+        public void onCloseLoadingView() {
+
+        }
+
+        public void onEnterComplete() {
+
+        }
+}
---
+++ base/src/main/java/cn/u313/plugin/base/UpdateCallback.java
@@ -0,0 +1,11 @@
+package cn.u313.plugin.base;
+
+public abstract class UpdateCallback {
+    public abstract void loadSessce();
+
+    /**
+     * 没有更新
+     */
+    public abstract void ukwn();
+    public abstract void loadError();
+}
